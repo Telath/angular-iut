5dac37df9dbbc325e98586be42e6e1d568bb9968
Telath <clement.pouillart@gmail.com>
2023-03-27 14:05:19 +0200

    ajout du package cors

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
+
 app.use('/', indexRouter);
 app.use('/students', studentRouter);
-
-app.use(cors());
 
 module.exports = app;
