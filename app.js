c181bb1b548daa5d0aec14e2f2192d779294ca3f
Telath <clement.pouillart@gmail.com>
2023-03-27 14:21:35 +0200

    Modif pour cors

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+
 var cors = require('cors');
-// ajout de cors
 
 var indexRouter = require('./routes/index');
 var studentRouter = require('./routes/student');
