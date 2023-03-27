18db0caf909bbfaae2c0597d8d6cb087f5faf7e5
Telath <clement.pouillart@gmail.com>
2023-03-27 15:00:11 +0200

    cors

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
-
 var cors = require('cors');
 
 var indexRouter = require('./routes/index');
