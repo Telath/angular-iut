fc8978fa9e9a92b4edc6ffd1d4c869614b44564b
Telath <clement.pouillart@gmail.com>
2023-03-27 14:14:16 +0200

    ajout du package cors - à jour

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var cors = require('cors');
+// ajout de cors
 
 var indexRouter = require('./routes/index');
 var studentRouter = require('./routes/student');
